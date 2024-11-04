259339623c20b46386657c84eaa09c924845567b
rkwkalrms <140880709+rkwkalrms@users.noreply.github.com>
2024-11-04 09:25:34 +0900

    feat : 입력 값에 대한 예외처리 및 반복 기능

--- src/main/java/lotto/InputManager.java
+++ src/main/java/lotto/InputManager.java
@@ -1,4 +1,58 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputManager {
+
+    public static int getPurchaseAmount() {
+        int amount = 0;
+        while (true) {
+            Messenger.howMuchPay();
+            String inputPurchaseAmount = Console.readLine();
+            try {
+                amount = Validator.checkType(inputPurchaseAmount);
+                Validator.checkAmount(amount);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return amount;
+    }
+
+    public static List<Integer> getWinningNumbers() {
+        List<Integer> winningNumbers = new ArrayList<>();
+        while (true) {
+            Messenger.writeWinningNumbers();
+            String inputWinningNumbers = Console.readLine();
+            Messenger.writePrintln();
+            try {
+                winningNumbers = Validator.checkAndParse(inputWinningNumbers);
+                break;
+            } catch(IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return winningNumbers;
+    }
+
+    public static int getBonusNumber() {
+        int bonusNumber = 0;
+        while (true) {
+            Messenger.writeBonusNumber();
+            String inputBonusNumber = Console.readLine();
+            Messenger.writePrintln();
+            try {
+                bonusNumber = Validator.checkType(inputBonusNumber);
+                Validator.checkBonusNumber(bonusNumber);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return bonusNumber;
+    }
 }
